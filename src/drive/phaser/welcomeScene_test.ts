eeaeee12cf21077eb785eac534c04269e482dc22
TungDT <tung.daothanhtung@gmail.com>
2019-11-13 19:51:56 +0700

    fix mocha only test first file

--- src/drive/phaser/welcomeScene_test.ts
+++ src/drive/phaser/welcomeScene_test.ts
@@ -1,7 +1,10 @@
+import * as rewire from "rewire";
+import * as assert from "assert";
+
 describe('TestWelcomeSceneAdd', function () {
     it('', function () {
-        let result = 5 + 2;
-        expect(result).toBe(7);
+        let result = 10 + 9;
+        assert.equal(result, 19)
     });
 });
 
